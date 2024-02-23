7becb08f595c2a8d89833309a676629a07e353f6
rafarizvi <152836959+rafarizvi@users.noreply.github.com>
2024-02-22 23:37:47 -0500

    added license options and updated readme

--- README.md
+++ README.md
@@ -2,10 +2,10 @@
 
 ## Description
 
-`The goal if this project is to ${purpose}. ${functionality}. ${result}.`
+The goal of this project is to create a readme.md file using the command line interface.
 
 ## Link to webpage
-[some weblink]()
+N/A
 
 ## Installation
 
@@ -13,24 +13,26 @@ N/A
 
 ## Usage
 
-`${usage}`
+Users are able answer the prompts presented for each section of the readme file. Then a readme.md file is generated
 
-![screenshot of password generator webpage](assets/images/webpage-screen-shot.png)
+## [Link to Video](https://drive.google.com/file/d/1-r_nAja_ABsTCj5hQo0GVrXzB7YK3zjY/view)
 
+![screenshot of readme generator](images/1.png)
+![screenshot of readme generator](images/2.png)
 ## Credits
 
 N/A
 
 ## License
 
-`This project uses the ${license}.`
+This project uses the MIT license
 
 ## Badges
 
 ![Static Badge](https://img.shields.io/badge/JavaScript-green)
-![Static Badge](https://img.shields.io/badge/HTML-CSS-blue)
-
 
 ## Features
 
-`${featureList}`
+- users are able to answer prompts.
+- prompts guide the user in an orderly fashion.
+- a readme file is generated in minutes.
--- index.js
+++ index.js
@@ -3,24 +3,18 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-
 // TODO: Create an array of questions for user input
-const inputs = ['Enter project title:','Describe your project:', 
-'Provide installation instructions (if any):', 'Provide usage information:', 
-'Select a license for your application:', 'What are the contribution guidelines:', 
-'What are the test instructions:', 'Enter your GitHub user name:', 'Enter your email address:'];
+const inputs = ['Enter project title:', 'Describe your project:',
+    'Provide installation instructions (if any):', 'Provide usage information:',
+    'Select a license for your application:', 'What are the contribution guidelines:',
+    'What are the test instructions:', 'Enter your GitHub user name:', 'Enter your email address:'];
 
 const [appName, description, installation, usage, license, contributing, tests, github, email] = inputs;
 
 // TODO: Create a function to write README file
-//function writeToFile(fileName, data) {}
-//const readMe = ({purpose}) =>
-
 const writeToFile = ({ appName, description, installation, usage, license, contributing, tests, github, email }) =>
 
-`# ${appName}
-
-### license badges.
+    `# ${appName}
 
 ![Static Badge](https://img.shields.io/badge/License-${license}-blue)
 
@@ -62,76 +56,73 @@ ${tests}
 ${email} `
 
 
-inquirer.prompt([
-    {
-        type: 'input',
-        name: 'appName',
-        message: appName,
-    },
+// TODO: Create a function to initialize app
+function init() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'appName',
+            message: appName,
+        },
 
-    {
-        type: 'input',
-        name: 'description',
-        message: description,
-    },
+        {
+            type: 'input',
+            name: 'description',
+            message: description,
+        },
 
-    {
-        type: 'input',
-        name: 'installation',
-        message: installation,
-    },
+        {
+            type: 'input',
+            name: 'installation',
+            message: installation,
+        },
 
-    {
-        type: 'input',
-        name: 'usage',
-        message: usage,
-    },
+        {
+            type: 'input',
+            name: 'usage',
+            message: usage,
+        },
 
-    {
-        type: 'list',
-        name: 'license',
-        message: license,
-        choices: ['MIT', 'Li1', 'Li2'],
-    },
+        {
+            type: 'list',
+            name: 'license',
+            message: license,
+            choices: ['MIT', 'Apache(2.0)', 'GPL(v3)', 'MozillaPublic(2.0)', 'BSD(3Clause)'],
+        },
 
-    {
-        type: 'input',
-        name: 'contributing',
-        message: contributing,
-    },
+        {
+            type: 'input',
+            name: 'contributing',
+            message: contributing,
+        },
 
-    {
-        type: 'input',
-        name: 'tests',
-        message: tests,
-    },
+        {
+            type: 'input',
+            name: 'tests',
+            message: tests,
+        },
 
-    {
-        type: 'input',
-        name: 'github',
-        message: github,
-    },
+        {
+            type: 'input',
+            name: 'github',
+            message: github,
+        },
 
-    {
-        type: 'input',
-        name: 'email',
-        message: email,
-    }
+        {
+            type: 'input',
+            name: 'email',
+            message: email,
+        }
 
-])
-    .then((answers) => {
+    ])
+        .then((answers) => {
 
-        const readMeContent = writeToFile(answers);
+            const readMeContent = writeToFile(answers);
 
-        fs.writeFile('newReadme2.md', readMeContent, (err) =>
-            err ? console.log(err) : console.log('Successfully created README.md!')
-        );
-    });
+            fs.writeFile('newReadme2.md', readMeContent, (err) =>
+                err ? console.log(err) : console.log('Successfully created README.md!')
+            );
+        });
+}
 
-// // TODO: Create a function to initialize app
-// function init() {
-//     writeToFile();
-// }
-
-
-// init();
+init();
