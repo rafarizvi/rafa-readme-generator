86ec81d70aec6bb835cb928eeabe5f470970da27
rafarizvi <152836959+rafarizvi@users.noreply.github.com>
2024-02-23 22:14:52 -0500

    updated code

--- index.js
+++ index.js
@@ -1,128 +1,97 @@
 
 // TODO: Include packages needed for this application
+// markdown module generates the readme file.
 const inquirer = require('inquirer');
 const fs = require('fs');
+const markdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
 const inputs = ['Enter project title:', 'Describe your project:',
     'Provide installation instructions (if any):', 'Provide usage information:',
     'Select a license for your application:', 'What are the contribution guidelines:',
-    'What are the test instructions:', 'Enter your GitHub user name:', 'Enter your email address:'];
+    'What are the test instructions:', 'Enter your GitHub user name:', 'Enter your email address:',
+    'Enter a file name:'];
 
-const [appName, description, installation, usage, license, contributing, tests, github, email] = inputs;
+const [appName, description, installation, usage, license, contributing, tests, github, email, fileName] = inputs;
 
-// TODO: Create a function to write README file
-const writeToFile = ({ appName, description, installation, usage, license, contributing, tests, github, email }) =>
-
-    `# ${appName}
-
-![Static Badge](https://img.shields.io/badge/License-${license}-blue)
-
-## <a id="descr"></a> Description
-
-${description}
-
-## Table of Contents
-* [Description](#descr)
-* [Installation](#install)
-* [Usage](#usg)
-* [License](#licen)
-* [Contributing ](#contr)
-* [Tests](#tests)
-* [Questions](#qst)
-
-## <a id="install"></a> Installation
-
-${installation}
-
-## <a id="usg"></a> Usage
-
-${usage}
-
-## <a id="licen"></a> License
-
-[!NOTE] 
-This project uses the ${license} License.
-
-## <a id="contr"></a> Contributing 
-${contributing}
-
-## <a id="tests"></a> Tests
-${tests}
-
-## <a id="qst"></a> Questions
-[${github}](https://github.com/${github})
-
-${email} `
-
-
-// TODO: Create a function to initialize app
-function init() {
+// TODO: Create a function to write README file.
+// Inquirer is used to prompt the user for questions.
+function writeToFile() {
     inquirer.prompt([
         {
             type: 'input',
             name: 'appName',
             message: appName,
         },
-
+    
         {
             type: 'input',
             name: 'description',
             message: description,
         },
-
+    
         {
             type: 'input',
             name: 'installation',
             message: installation,
         },
-
+    
         {
             type: 'input',
             name: 'usage',
             message: usage,
         },
-
+    
         {
             type: 'list',
             name: 'license',
             message: license,
-            choices: ['MIT', 'Apache(2.0)', 'GPL(v3)', 'MozillaPublic(2.0)', 'BSD(3Clause)'],
+            choices: ['MIT', 'Apache(2.0)', 'GPL(v3)', 'MozillaPublic(2.0)', 'BSD(3Clause)', 'N/A'],
         },
-
+    
         {
             type: 'input',
             name: 'contributing',
             message: contributing,
         },
-
+    
         {
             type: 'input',
             name: 'tests',
             message: tests,
         },
-
+    
         {
             type: 'input',
             name: 'github',
             message: github,
         },
-
+    
         {
             type: 'input',
             name: 'email',
             message: email,
-        }
+        },
 
+        {
+            type: 'input',
+            name: 'fileName',
+            message: fileName,
+        }
+    
     ])
         .then((answers) => {
-
-            const readMeContent = writeToFile(answers);
-
-            fs.writeFile('newReadme2.md', readMeContent, (err) =>
+            const readMeContent = markdown.generateMarkdown(answers);
+    
+            fs.writeFile(`${answers.fileName}.md`, readMeContent, (err) =>
                 err ? console.log(err) : console.log('Successfully created README.md!')
             );
         });
+}
+
+// TODO: Create a function to initialize app
+function init() {
+    writeToFile();
 }
 
 init();
